f3f23fc77741ce6d4ea405db30033ae1f557b4fc
KasiaJakubowska <katarzyna.jakubowska1998@gmail.com>
2019-03-20 20:27:34 +0100

    New

---
+++ .history/maina_20190302120357.js
@@ -0,0 +1,15 @@
+const square = document.createElement('div');
+document.body.appendChild(square);
+
+let size = 100; //wielkość kwadratu
+square.style.width = size +"px";
+square.style.height = size +"px";
+
+//maksymalna wielkość kwadratu
+//window.innerWidth * 0.5
+
+window.addEventListener("scroll", function(){
+    size += 10
+    square.style.width = size + "px";
+    square.style.height = size + "px";
+})
---
+++ .history/maina_20190302120554.js
@@ -0,0 +1,17 @@
+const square = document.createElement('div');
+document.body.appendChild(square);
+
+let size = 100; //wielkość kwadratu
+square.style.width = size +"px";
+square.style.height = size +"px";
+
+//maksymalna wielkość kwadratu
+//window.innerWidth * 0.5
+
+window.addEventListener("scroll", function(){
+    if(size<window.innerWidth * 0.5){
+    size += 5
+    square.style.width = size + "px";
+    square.style.height = size + "px";
+    }
+})
